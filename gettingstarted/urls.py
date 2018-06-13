fabfd74b9ec97e549294000895b70d695451cfb7
Ikaro Pinheiro <ikaro.lopes@dafiti.com.br>
2018-06-13 17:34:00 -0300

    - Create a test route - Remove html ganarated from heroku

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -12,6 +12,7 @@ import hello.views
 
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
+    url(r'^lerox/', hello.view.lerox, name='lerox'),
     url(r'^db', hello.views.db, name='db'),
     path('admin/', admin.site.urls),
 ]
--- hello/views.py
+++ hello/views.py
@@ -8,6 +8,9 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
+def lerox(request):
+	return render(request, 'lerox.html')
+
 
 def db(request):
 
@@ -17,4 +20,3 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
-
