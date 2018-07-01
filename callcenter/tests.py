26e6c6fa349318a0d49e57e09bda10ee287b2c4d
Ikaro Pinheiro <ikaropinheiro6@gmail.com>
2018-07-01 02:41:18 -0300

    Fix unit test and create travis file to CI

--- callcenter/tests.py
+++ callcenter/tests.py
@@ -7,12 +7,12 @@ class CallRecordTest(TestCase):
     def setUp(self):
         # Every test needs access to the request factory.
         self.call_record = mommy.make(
-            CallRecord, type='end', timestamp='2018-11-25 08:08:08',
+            CallRecord, type=2, timestamp='2018-11-25 08:08:08',
             call_id=50, source='', destination=''
         )
 
     def test_call_record_creation(self):
         self.assertTrue(isinstance(self.call_record, CallRecord))
-        self.assertEquals(self.call_record.type, 'end')
+        self.assertEquals(self.call_record.type, 2)
         self.assertEquals(self.call_record.call_id, 50)
         self.assertEquals(self.call_record.timestamp, '2018-11-25 08:08:08')
