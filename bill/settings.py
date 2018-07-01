9adf4daf41ea37f844ccaae78ff77d81ac370a32
Ikaro Pinheiro <ikaropinheiro6@gmail.com>
2018-07-01 02:53:48 -0300

    Fix dependencys to build project

--- bill/settings.py
+++ bill/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'callcenter',
-        'USER': 'root',
+        'USER': 'postgres',
         'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT':'',
