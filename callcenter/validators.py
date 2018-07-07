0dabcaa8f7e1787c58405d741ebebb9ab1060a49
Ikaro Pinheiro <ikaropinheiro6@gmail.com>
2018-07-06 21:12:39 -0300

    Create a date validation
    - Validate month and year seted by user on GET bill
    - Create exception if date is not valid
    - Unit tests to BillDateValidator

--- callcenter/validators.py
+++ callcenter/validators.py
@@ -1,4 +1,7 @@
+from datetime import datetime
 from .models import CallRecord
+from .exceptions import InvalidBillDate
+import dateutil.relativedelta
 
 START_TYPE = 1
 END_TYPE = 2
@@ -44,3 +47,45 @@ class BillValidator:
         bill_data['year'] = int(end_call_datetime.year)
 
         return bill_data
+
+
+class BillDateValidator:
+    """
+    Class to make all date validation
+    """
+    actual_date = datetime.now().date()
+
+    def validate_bill_date(self, month=None, year=None):
+        if month and year:
+            self.__make_validation(int(month), int(year))
+        else:
+            self.__get_previous_month()
+
+        return {
+            'month': self.month,
+            'year': self.year
+        }
+
+    def __make_validation(self, month, year):
+        if (year > 9999 or month > 12)\
+                or (year < 0 or month < 1):
+            raise InvalidBillDate("Insert a valid date")
+
+        if year > self.actual_date.year:
+            raise InvalidBillDate("Year requested is bigger than actual")
+
+        if (year == self.actual_date.year and month == self.actual_date.month)\
+                or (year == self.actual_date.year
+                    and self.actual_date.month == 1):
+            raise InvalidBillDate("The bill to this month isn't closed yet")
+
+        self.month = month
+        self.year = year
+
+    def __get_previous_month(self):
+        last_month = self.actual_date - dateutil.relativedelta.relativedelta(
+            months=1
+        )
+
+        self.month = last_month.month
+        self.year = last_month.year
