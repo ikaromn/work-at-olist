3fd0ea2d0e5e089e819f73840a31200ced7c2dd6
Ikaro Pinheiro <ikaropinheiro6@gmail.com>
2018-07-08 19:12:14 -0300

    Change django timezone to America/Sao_Paulo

--- bill/settings.py
+++ bill/settings.py
@@ -114,13 +114,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
